6ca6a4e09aa9a0474c704d0dce2135f0c467b2cc
Jvelez10 <74399879+Jvelez10@users.noreply.github.com>
2023-01-15 23:15:18 -0500

    Update and rename navigation.js to addNavigation.js

--- web215/scripts/addNavigation.js
+++ web215/scripts/addNavigation.js
@@ -1,40 +1,45 @@
-var pages = {
+var pages = { // declaring navigation pages; key - name on Page : value - link to page
 	Home : "index.html",
 	Introduction : "introduction.html",
-	Contract : "contract.html",
-	Brand : "brand.html"
-};
+	Contract : "contract.html"//,
+	//Brand : "brand.html",
+	//TOC : "toc.html",
+	//"One Page" : "onepage.html"
+}
 var leveNavlOffset = "";
 
-function addLogo(navigation) 
+function addLogo(navigation) // adding logo (in this case just test with link to starting page)
 {
+	//var navigation = document.getElementById("navigation"); // get element with navigation id
 	
 	var logo = document.createElement("div"); // create div element
 	var a = document.createElement("a");			
 	var logoLink  = leveNavlOffset + "index.html";
 
 
-	a.setAttribute("href", logoLink); 
+	a.setAttribute("href", logoLink); // setting ling to index page
 	var headerTemp = document.createElement("h1");
-	headerTemp.appendChild(document.createTextNode("Joseph Velez's WEB115 | Silverbeam Industries"));
-	a.appendChild(headerTemp); 
+	headerTemp.appendChild(document.createTextNode("Alexei's WEB215"));
+	a.appendChild(headerTemp); // adding created elelemnts together
 	logo.appendChild(a);
 	navigation.appendChild(logo);
 }
 function addNavigationBar(navigation)
 {
+	//var navigation = document.getElementById("navigation");
 	var navUl = document.createElement("ul");
 	for(var key in pages)
 	{
 		var value = pages[key];				
-		var navLi = document.createElement("li");
+		var navLi = document.createElement("li")
 		var a = document.createElement("a");
 		
 		value = leveNavlOffset + value;
 
 		a.setAttribute("href", value);
 		
-		if (location.pathname.indexOf(value) > -1) { a.setAttribute("class", "active"); } 
+		if (location.pathname.indexOf(value) > -1) { a.setAttribute("class", "active"); } // finding if current page contains added link, if yes add class for CSS
+		
 		a.appendChild(document.createTextNode(key));
 		navLi.appendChild(a);
 		navUl.appendChild(navLi);				
