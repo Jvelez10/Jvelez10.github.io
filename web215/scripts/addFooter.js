6249f6217bcd2c4f1e3386cc30bafbff0c78e272
Jvelez10 <74399879+Jvelez10@users.noreply.github.com>
2023-01-15 23:15:37 -0500

    Update addFooter.js

--- web215/scripts/addFooter.js
+++ web215/scripts/addFooter.js
@@ -1,65 +1,79 @@
-var pages = { // declaring navigation pages; key - name on Page : value - link to page
-	Home : "index.html",
-	Introduction : "introduction.html",
-	Contract : "contract.html"//,
-	//Brand : "brand.html",
-	//TOC : "toc.html",
-	//"One Page" : "onepage.html"
-}
-var leveNavlOffset = "";
+var footerLinks = {
+	"Github" : "https://github.com/asimchi0",
+	"Github Page" : "https://asimchi0.github.io/",
+	"Github WEB215" : "https://asimchi0.github.io/web215/",
+	"Freecodecamp" : "https://www.freecodecamp.org/asimchi0",
+	"CodeAcademy" : "https://www.codecademy.com/profiles/asimchi0",
+	"JSFiddle" : "https://jsfiddle.net/user/asimchi0/fiddles/",
+	"LinkedIn Learning" : "https://www.linkedin.com/learning/?u=76141674"
+};
+var levelFooterOffset = "";
 
-function addLogo(navigation) // adding logo (in this case just test with link to starting page)
+function addValImage(src,alt)
 {
-	//var navigation = document.getElementById("navigation"); // get element with navigation id
+	var img = document.createElement("img");
+	img.setAttribute("src",src);
+	img.setAttribute("alt",alt);
+	img.setAttribute("height","31");
+	img.setAttribute("width","88");
+	img.setAttribute("style","border:0px;");
+	return img;
+}
+
+function addValidation(footer)
+{
+	var validation = document.createElement("div");
+	var html = document.createElement("a");
+	html.setAttribute("href", "https://validator.w3.org/check?uri=" + location.href)
+	html.appendChild(addValImage(levelFooterOffset +"images/button_validation_html5.png", "Valid HTML 5"));
+	var css = document.createElement("a");
+	css.setAttribute("href", "https://jigsaw.w3.org/css-validator/validator?uri=" + location.href);
+	css.appendChild(addValImage(levelFooterOffset + "images/button_validation_css.png", "Valid CSS"));
+	validation.appendChild(html);
+	validation.appendChild(css);
+	footer.appendChild(validation)
+}
+
+function addDesignBy(footer)
+{
+	var design = document.createElement("div");
+	design.appendChild(document.createTextNode("Page built by NoNameCorp"));
+	footer.appendChild(design);
+}
+
+function addLinks(footer)
+{
+	var links = document.createElement("div");
+	// extra code to not add separator for last element.
+	var dictLength = Object.keys(footerLinks).length;
+	var lastkey = Object.keys(footerLinks)[dictLength-1];
 	
-	var logo = document.createElement("div"); // create div element
-	var a = document.createElement("a");			
-	var logoLink  = leveNavlOffset + "index.html";
-
-
-	a.setAttribute("href", logoLink); // setting ling to index page
-	var headerTemp = document.createElement("h1");
-	headerTemp.appendChild(document.createTextNode("Alexei's WEB215"));
-	a.appendChild(headerTemp); // adding created elelemnts together
-	logo.appendChild(a);
-	navigation.appendChild(logo);
-}
-function addNavigationBar(navigation)
-{
-	//var navigation = document.getElementById("navigation");
-	var navUl = document.createElement("ul");
-	for(var key in pages)
+	for (var key in footerLinks)
 	{
-		var value = pages[key];				
-		var navLi = document.createElement("li")
+		var value = footerLinks[key];
 		var a = document.createElement("a");
-		
-		value = leveNavlOffset + value;
-
-		a.setAttribute("href", value);
-		
-		if (location.pathname.indexOf(value) > -1) { a.setAttribute("class", "active"); } // finding if current page contains added link, if yes add class for CSS
-		
-		a.appendChild(document.createTextNode(key));
-		navLi.appendChild(a);
-		navUl.appendChild(navLi);				
-	}			
-	navigation.appendChild(navUl);
+		a.setAttribute("href",value);
+		a.appendChild(document.createTextNode(key));				
+		links.appendChild(a);
+		if(key != lastkey) links.appendChild(document.createTextNode(" | "));
+	}
+	footer.appendChild(links);
 }
-function addNavigation(level = 0)
+
+function addFooter(level = 0)
 {
 	for (var i = 0; i < level; i++)
 	{
-		leveNavlOffset = "../" + leveNavlOffset;
+		levelFooterOffset = "../" + levelFooterOffset;
 	}	
 	
 	var body = document.body;
-	var headerNav = document.createElement("header");
-	var navigation = document.createElement("nav");
-	
-	addLogo(navigation, level);
-	addNavigationBar(navigation, level);
-	
-	headerNav.appendChild(navigation);			
-	body.appendChild(headerNav);
+	var footer = document.createElement("footer");
+	addLinks(footer);
+	footer.appendChild(document.createElement("br"));
+	footer.appendChild(document.createElement("br"));
+	addDesignBy(footer);
+	footer.appendChild(document.createElement("br"));
+	addValidation(footer, level);			
+	body.appendChild(footer);
 }
