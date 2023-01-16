0be3aee98b838ea22e11b57fd1596bc455222984
Jvelez10 <74399879+Jvelez10@users.noreply.github.com>
2023-01-15 22:48:28 -0500

    Create navigation.js

---
+++ web215/scripts/navigation.js
@@ -0,0 +1,60 @@
+var pages = {
+	Home : "index.html",
+	Introduction : "introduction.html",
+	Contract : "contract.html",
+	Brand : "brand.html"
+};
+var leveNavlOffset = "";
+
+function addLogo(navigation) 
+{
+	
+	var logo = document.createElement("div"); // create div element
+	var a = document.createElement("a");			
+	var logoLink  = leveNavlOffset + "index.html";
+
+
+	a.setAttribute("href", logoLink); 
+	var headerTemp = document.createElement("h1");
+	headerTemp.appendChild(document.createTextNode("Joseph Velez's WEB115 | Silverbeam Industries"));
+	a.appendChild(headerTemp); 
+	logo.appendChild(a);
+	navigation.appendChild(logo);
+}
+function addNavigationBar(navigation)
+{
+	var navUl = document.createElement("ul");
+	for(var key in pages)
+	{
+		var value = pages[key];				
+		var navLi = document.createElement("li");
+		var a = document.createElement("a");
+		
+		value = leveNavlOffset + value;
+
+		a.setAttribute("href", value);
+		
+		if (location.pathname.indexOf(value) > -1) { a.setAttribute("class", "active"); } 
+		a.appendChild(document.createTextNode(key));
+		navLi.appendChild(a);
+		navUl.appendChild(navLi);				
+	}			
+	navigation.appendChild(navUl);
+}
+function addNavigation(level = 0)
+{
+	for (var i = 0; i < level; i++)
+	{
+		leveNavlOffset = "../" + leveNavlOffset;
+	}	
+	
+	var body = document.body;
+	var headerNav = document.createElement("header");
+	var navigation = document.createElement("nav");
+	
+	addLogo(navigation, level);
+	addNavigationBar(navigation, level);
+	
+	headerNav.appendChild(navigation);			
+	body.appendChild(headerNav);
+}
